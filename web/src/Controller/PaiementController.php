184cb8f1b89133ba507a42e25e76284943464eba
youssefsouilem <souilemyoussef0@gmail.com>
2026-02-10 05:29:48 +0100

    titulaire-facture-paiement
    `Refactored controllers and templates to remove patient-specific routes and templates, replacing them with titulaire-specific routes and templates`

--- web/src/Controller/PaiementController.php
+++ web/src/Controller/PaiementController.php
@@ -1,7 +1,7 @@
 <?php
 
-namespace App\Controller;
 
+namespace App\Controller;
 use App\Entity\Paiement;
 use App\Form\PaiementType;
 use App\Repository\PaiementRepository;
@@ -11,18 +11,18 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/paiement')]
+#[Route('/titulaire/paiement')]
 final class PaiementController extends AbstractController
 {
-    #[Route(name: 'app_paiement_index', methods: ['GET'])]
+    #[Route('/', name: 'app_patient_paiement_index', methods: ['GET'])]
     public function index(PaiementRepository $paiementRepository): Response
     {
-        return $this->render('paiement/index.html.twig', [
+        return $this->render('titulaire/paiement/index.html.twig', [
             'paiements' => $paiementRepository->findAll(),
         ]);
     }
 
-    #[Route('/new', name: 'app_paiement_new', methods: ['GET', 'POST'])]
+    #[Route('/new', name: 'app_patient_paiement_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $paiement = new Paiement();
@@ -32,50 +32,21 @@ final class PaiementController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->persist($paiement);
             $entityManager->flush();
-
-            return $this->redirectToRoute('app_paiement_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_patient_paiement_index');
         }
 
-        return $this->render('paiement/new.html.twig', [
+        return $this->render('titulaire/paiement/new.html.twig', [
             'paiement' => $paiement,
             'form' => $form,
         ]);
     }
 
-    #[Route('/{id}', name: 'app_paiement_show', methods: ['GET'])]
+    #[Route('/{id}', name: 'app_patient_paiement_show', methods: ['GET'])]
     public function show(Paiement $paiement): Response
     {
-        return $this->render('paiement/show.html.twig', [
+        return $this->render('titulaire/paiement/show.html.twig', [
             'paiement' => $paiement,
         ]);
     }
-
-    #[Route('/{id}/edit', name: 'app_paiement_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Paiement $paiement, EntityManagerInterface $entityManager): Response
-    {
-        $form = $this->createForm(PaiementType::class, $paiement);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_paiement_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('paiement/edit.html.twig', [
-            'paiement' => $paiement,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'app_paiement_delete', methods: ['POST'])]
-    public function delete(Request $request, Paiement $paiement, EntityManagerInterface $entityManager): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$paiement->getId(), $request->getPayload()->getString('_token'))) {
-            $entityManager->remove($paiement);
-            $entityManager->flush();
-        }
-
-        return $this->redirectToRoute('app_paiement_index', [], Response::HTTP_SEE_OTHER);
-    }
+    // No Edit or Delete here!
 }
