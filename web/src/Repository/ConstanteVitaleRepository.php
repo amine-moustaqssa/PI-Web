c4bd86d66f7fab6e938d5d19cd5ef448fd87267c
Ghass3n <hmidamohamedghassen@gmail.com>
2026-02-10 13:55:05 +0100

    feat(web): added the infirmier dashboard

--- web/src/Repository/ConstanteVitaleRepository.php
+++ web/src/Repository/ConstanteVitaleRepository.php
@@ -16,28 +16,45 @@ class ConstanteVitaleRepository extends ServiceEntityRepository
         parent::__construct($registry, ConstanteVitale::class);
     }
 
-//    /**
-//     * @return ConstanteVitale[] Returns an array of ConstanteVitale objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('c')
-//            ->andWhere('c.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('c.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    /**
+     * Count constantes vitales recorded today.
+     */
+    public function countToday(): int
+    {
+        $start = new \DateTime('today 00:00:00');
+        $end = new \DateTime('today 23:59:59');
 
-//    public function findOneBySomeField($value): ?ConstanteVitale
-//    {
-//        return $this->createQueryBuilder('c')
-//            ->andWhere('c.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+        return (int) $this->createQueryBuilder('c')
+            ->select('COUNT(c.id)')
+            ->andWhere('c.date_prise BETWEEN :start AND :end')
+            ->setParameter('start', $start)
+            ->setParameter('end', $end)
+            ->getQuery()
+            ->getSingleScalarResult();
+    }
+
+    //    /**
+    //     * @return ConstanteVitale[] Returns an array of ConstanteVitale objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('c')
+    //            ->andWhere('c.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('c.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
+
+    //    public function findOneBySomeField($value): ?ConstanteVitale
+    //    {
+    //        return $this->createQueryBuilder('c')
+    //            ->andWhere('c.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
 }
